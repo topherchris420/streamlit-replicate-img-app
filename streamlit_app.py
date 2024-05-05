7776a0edd8e038f213aea9db8554af459aa71151
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-05-05 17:21:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Vers3Dynamics",
                    page_icon="🎨",
                    layout="wide")
 icon.show_icon("📡")
-st.markdown("# :rainbow[Vers3Dynamics: Reflections in Pixels")
+st.markdown("# :rainbow[Vers3Dynamics: Reflections in Pixels]")
 
 # API Tokens and endpoints from `.streamlit/secrets.toml` file
 REPLICATE_API_TOKEN = st.secrets["REPLICATE_API_TOKEN"]
