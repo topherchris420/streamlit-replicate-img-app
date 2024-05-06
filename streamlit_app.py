3de32ccd9b50e80eb63bc9e22e5c46289373e22d
ciao_chris <93827718+topherchris420@users.noreply.github.com>
2024-05-06 15:08:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ def configure_sidebar() -> None:
         st.markdown(
             """
             ---
-            Join the journey at:
+            Start the journey at:
 
             Introduction🌐[Preface](https://share.synthesia.io/064c0de8-097a-4d93-82bf-92b741021b84)
             
